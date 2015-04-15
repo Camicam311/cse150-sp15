ace415985db0bb3dda9eccfe293c4c4b3a431f33
Chris Weller <chriskweller@gmail.com>
2015-04-15 12:47:45 -0700

    Added basic test script.

---
+++ pa1/test.py
@@ -0,0 +1,14 @@
+import os
+
+print "PA COUNT: " + str(len([x for x in os.listdir(".") if '.py' in x]))
+print "TEST COUNT: " + str(len([x for x in os.listdir("tests") if '.txt' in x]))
+
+for filename in [x for x in os.listdir(".") if '.py' in x]:
+    if 'assignment1_p1.py' == filename or 'test.py' == filename:
+        continue
+    for test in [x for x in os.listdir("tests") if '.txt' in x]:
+        print (filename + " " +  test)
+        os.system("python " + filename + "< tests/" + test)
+        print ""
+    
+
