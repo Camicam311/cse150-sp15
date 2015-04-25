4cbee000d931507ec5ff1b8a30f35b9ec0b1dc7e
Chris Weller <chriskweller@gmail.com>
2015-04-25 00:32:35 -0700

    Got MiniMax functional, but I'm pretty sure it's not working correctly. Investigate later and lemme know what you think.

--- pa2/solutions/p1_minimax_player.py
+++ pa2/solutions/p1_minimax_player.py
@@ -3,7 +3,6 @@ __author__ = 'Sivasubramanian Chandrasegarampillai, Walter Curnow'
 __email__ = 'rchandra@uci.edu,wcurnow@uci.edu'
 
 from assignment2 import Player, State, Action
-
 class MinimaxPlayer(Player):
     def move(self, state):
         """Calculates the best move from the given board using the minimax algorithm.
@@ -15,5 +14,30 @@ class MinimaxPlayer(Player):
             the next move (Action)
         """
 
-        # TODO implement this
-        return state.actions()[0]
+        # internal function because otherwise python throws a hissy fit
+        # (geddit, hissy fit, that's a pun)
+
+        # I think this shit is busted. It returns moves, but I managed to
+        # beat it, which isn't a good sign. We need to investigate further.
+        # @itsweller is the one that fuckered this all up
+        '''
+        Args:
+            state (State): The current state of the board.
+            min_or_max (String): Whether we're to perform min or max stage,
+                                 accepted values are 'min' and 'max'
+        '''
+        def minimax_play(state, min_or_max):
+            if state.is_terminal():
+                return state.utility(self)
+            if min_or_max == 'min':
+                return min([(minimax_play(state.result(possibility),'max'), 
+                    possibility) for possibility in state.actions()], 
+                    key=lambda x:x[0])
+            if min_or_max == 'max':
+                return max([(minimax_play(state.result(possibility),'min'), 
+                    possibility) for possibility in state.actions()], 
+                    key=lambda x:x[0])
+
+        return max([(minimax_play(state.result(possibility),'min'), possibility) for possibility in state.actions()],key=lambda x:x[0])[1]
+
+
