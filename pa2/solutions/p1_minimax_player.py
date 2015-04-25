178f8d585530dae59bd0aa6da8d1dc0fe7a8b168
Chris Weller <chriskweller@gmail.com>
2015-04-25 02:30:40 -0700

    I'm not happy about this.

--- pa2/solutions/p1_minimax_player.py
+++ pa2/solutions/p1_minimax_player.py
@@ -31,18 +31,37 @@ class MinimaxPlayer(Player):
         Args:
             state (State): The current state of the board.
         '''
+        global move
+        move = None
         def minimax_play(state):
+            global move
             if state.is_terminal():
                 return state.utility(self)
-            if state.to_play == self: # Maximize for us
-                return max([(minimax_play(state.result(possibility)), 
-                    possibility) for possibility in state.actions()], 
-                    key=lambda x:x[0])
-            else:                     # Minimize for them 
-                return min([(minimax_play(state.result(possibility)), 
-                    possibility) for possibility in state.actions()], 
-                    key=lambda x:x[0])
 
-        return  minimax_play(state)[1]
+            ls = []
+            if state.to_play == self: # Maximize for us
+                for possibility in state.actions():
+                    ls.append((possibility,minimax_play(state.result(possibility))))
+                current_largest = None,-2
+                for x in ls:
+                    if x[1] > current_largest[1]:
+                        current_largest = x[0],x[1]
+                        move = current_largest[0]
+                print current_largest[1]
+                return current_largest[1]
+
+            else:                     # Minimize for them 
+                for possibility in state.actions():
+                    ls.append((possibility,minimax_play(state.result(possibility))))
+                current_largest = None,2
+                for x in ls:
+                    if x[1] < current_largest[1]:
+                        current_largest = x[0],x[1]
+                        move = current_largest[0]
+                print current_largest[1]
+                return current_largest[1]
+
+        minimax_play(state)
+        return move
 
 
