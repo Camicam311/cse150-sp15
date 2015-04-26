94b3bc6ce2b9cbeb8929940753a5d96f5953b57b
Chris Weller <chriskweller@gmail.com>
2015-04-26 11:21:42 -0700

    Implemented transposition tables, 80 char lines, switch to native infinity.

--- pa2/solutions/p2_alphabeta_player.py
+++ pa2/solutions/p2_alphabeta_player.py
@@ -3,6 +3,7 @@ __author__ = 'Sivasubramanian Chandrasegarampillai, Walter Curnow'
 __email__ = 'rchandra@uci.edu,wcurnow@uci.edu'
 
 from assignment2 import Player, State, Action
+from collections import defaultdict
 
 class AlphaBetaPlayer(Player):
     def move(self,state):
@@ -11,7 +12,7 @@ class AlphaBetaPlayer(Player):
         bestMove = 0
         move = None
 
-        tTable = {}         #Transposition table
+        tTable = defaultdict(lambda: False)         #Transposition table
 
         def max_value(state, alpha, beta):
             global move
@@ -20,9 +21,16 @@ class AlphaBetaPlayer(Player):
             if state.is_terminal():
                 return state.utility(self)
 
-            current_best = -9999999999
-            for (next_state, poss) in [(state.result(possibility),possibility) for possibility in state.actions()]:
-                current_best = max(current_best, min_value(next_state, alpha, beta))
+            current_best = float("-inf")
+            for (next_state, poss) in [(state.result(possibility),
+                possibility) for possibility in state.actions()]:
+                
+                if tTable[next_state]:
+                    continue
+                else: tTable[next_state] = True
+
+                current_best = max(min_value(next_state, alpha, beta),
+                        current_best)
 
                 if current_best >= beta:
                     return current_best
@@ -42,9 +50,18 @@ class AlphaBetaPlayer(Player):
             if state.is_terminal():
                 return state.utility(self)
 
-            current_best = 9999999999
-            for (next_state, poss) in [(state.result(possibility),possibility) for possibility in state.actions()]:
-                current_best = min(current_best, max_value(next_state, alpha, beta))
+            current_best = float("inf")
+            for (next_state, poss) in [(state.result(possibility),
+                possibility) for possibility in state.actions()]:
+
+                if tTable[next_state]:
+                    continue
+                else:
+                    tTable[next_state] = True
+
+                current_best = min(max_value(next_state, alpha, beta), 
+                        current_best)
+
                 if current_best <= alpha:
                     return current_best
 
@@ -57,7 +74,7 @@ class AlphaBetaPlayer(Player):
             return current_best
 
 
-        current_best = max_value(state, -9999999999, 9999999999)
+        current_best = max_value(state, float("-inf"), float("inf"))
         print("The move is ", move, "And utility: ", current_best)
         move.color = 1
         return move
