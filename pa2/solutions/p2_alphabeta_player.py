843b6316d88fca196baeb63f02ae865d8ab4c91a
Chris Weller <chriskweller@gmail.com>
2015-05-06 20:07:13 -0700

    Removed old code.

--- pa2/solutions/p2_alphabeta_player.py
+++ pa2/solutions/p2_alphabeta_player.py
@@ -15,83 +15,6 @@ class AlphaBetaPlayer(Player):
         Returns:
             the next move (Action)
         """
-
-        # internal function because otherwise python throws a hissy fit
-        # (geddit, hissy, python, that's a pun)
-
-        '''
-        Args:
-            state (State): The current state of the board.
-        '''
-        '''
-        global move
-        move = None
-        prevScore = 0
-
-        tTable = defaultdict(lambda: None)         #Transposition table
-
-        def minimax_play(state, alpha, beta):
-            global move
-            global lastScore
-            current_best = None
-
-            #if state.is_terminal():
-            #    return state.utility(self)
-
-            if (tTable.get(state) == None):
-                #tTable[state] = True
-
-                if state.is_terminal():
-                    return state.utility(self)
-
-                if state.to_play == self: # Maximize for us
-                    current_best = -2
-
-                    for (score,poss) in [(minimax_play(state.result(possibility),
-                        alpha, beta), possibility) for possibility 
-                        in state.actions()]:
-
-                        #lastScore = score
-                        if(tTable[state] < score):
-                            tTable[state] = score
-
-                        if score >= beta:
-                            return tTable[state]
-
-                        alpha = max(score, alpha)
-
-                        if score > current_best:
-                            current_best = score
-                            move = poss
-
-                else:                     # Minimize for them
-                    current_best = 2
-                    
-                    for (score,poss) in [(minimax_play(state.result(possibility),
-                        alpha, beta), possibility) for possibility 
-                        in state.actions()]:
-
-                        if(tTable[state] > score):
-                            tTable[state] = score
-
-                        if score <= alpha:
-                            return tTable[state]
-
-                        beta = min(score, beta)
-
-                        if score < current_best:
-                            current_best = score
-                            move = poss
-
-                return current_best
-
-
-            else:
-                return tTable[state]
-
-        #tTable[state] = True
-        '''
-
         tTable = defaultdict(lambda: None)         #Transposition table
 
         def alpha_beta_search(state):
@@ -115,6 +38,7 @@ class AlphaBetaPlayer(Player):
                 tTable[state] = max(tTable[state], v)
 
                 if v >= beta:
+                    print "Pruning"
                     return v
                 alpha = max(alpha, v)
             return v
@@ -131,6 +55,7 @@ class AlphaBetaPlayer(Player):
                 tTable[state] = max(tTable[state], v)
 
                 if v <= alpha:
+                    print "Pruning"
                     return v
                 beta = min(beta, v)
             return v
