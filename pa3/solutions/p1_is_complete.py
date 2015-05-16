3e882029c694963f3a8109d56a64e09cb8522672
Rene Sanchez <risanche@ucsd.edu>
2015-05-16 11:01:22 -0700

    p1 done

--- pa3/solutions/p1_is_complete.py
+++ pa3/solutions/p1_is_complete.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Please write your names, separated by commas.'
-__email__ = 'Please write your email addresses, separated by commas.'
+__author__ = 'Rene Sanchez, Chris Weller'
+__email__ = 'risanche@ucsd.edu, chriskweller@gmail.com'
 
+from assignment3 import *
 
 def is_complete(csp):
     """Returns True when the CSP assignment is complete, i.e. all of the variables in the CSP have values assigned."""
@@ -10,6 +11,8 @@ def is_complete(csp):
     # Also, if the variable is assigned, variable.is assigned() will be True.
     # (Note that this can happen either by explicit assignment using variable.assign(value),
     # or when the domain of the variable has been reduced to a single value.)
+    for variable in csp.variables:
+        if variable.is_assigned() == False:
+            return False
 
-    # TODO implement this
-    pass
+    return True
