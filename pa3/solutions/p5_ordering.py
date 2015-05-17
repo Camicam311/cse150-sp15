743f494a83ab583753be7cd270597eac9c6963e9
Rene Sanchez <risanche@ucsd.edu>
2015-05-17 16:26:36 -0700

    Buggy 1st half of p5, incorrect output

--- pa3/solutions/p5_ordering.py
+++ pa3/solutions/p5_ordering.py
@@ -3,6 +3,25 @@ __author__ = 'Rene Sanchez, Chris Weller'
 __email__ = 'risanche@ucsd.edu, chriskweller@gmail.com'
 
 from Queue import PriorityQueue
+from collections import defaultdict
+
+def get_available_domain(csp, variable, value):
+    global numCons
+
+    for cons in csp.constraints[variable]: #Iterate over neighbors of var
+        if cons.var2.domain == 1:
+            if cons.is_satisfied(value, cons.var2.value) == False: #If this variable's value breaks the
+                return -1                                      # constraint with a neighbor
+        else:
+            numCons +=1
+            counter = 0
+            for value2 in cons.var2.domain:
+                if cons.is_satisfied(value, value2) == False:
+                    counter += 1
+            if counter == cons.var2.domain:
+                return -1
+
+    return 0
 
 def select_unassigned_variable(csp):
     """Selects the next unassigned variable, or None if there is no more unassigned variables
@@ -13,9 +32,24 @@ def select_unassigned_variable(csp):
     then it picks the variable that is involved in the largest number of constraints on other
     unassigned variables.
     """
-    pass
+    global numCons
+    numCons = 0
 
+    q = PriorityQueue()
+    for var in csp.variables:
+        counter = 0
+        if var.is_assigned == False:
+            counter = 0
+            for val in val.domain:
+                counter += get_available_domain(csp,var,val)
+        q.put((len(var.domain) + counter + 1, numCons, var))
+        numCons = 0
 
+    if(q):
+        var = q.get()[2]
+        return var
+    else:
+        return None
 
 def order_domain_values(csp, variable):
     """Returns a list of (ordered) domain values for the given variable.
