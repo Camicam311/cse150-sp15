b28ef86ea1ad52c761411d1a2146760de502dacc
Rene Sanchez <risanche@ucsd.edu>
2015-05-25 09:13:48 -0700

    Updated p6 with new code for p1-p5

--- pa3/solutions/p6_solver.py
+++ pa3/solutions/p6_solver.py
@@ -16,24 +16,34 @@ def inference(csp, variable):
     """
     return ac3(csp, csp.constraints[variable].arcs())
 
+#Helper method for select_unassigned_variable, checks if a var-val combination violates
+# neighboring constraints.And returns by how much we should reduce our variable's domain by.
+#Input: A csp, a variable from said csp, a value from said variable
+#Output: 0 if the value doesn't violate any constraints, -1 otherwise.
 def get_available_domain(csp, variable, value):
     global numCons
 
+    reduceDomainBy = 0
+
     for cons in csp.constraints[variable]: #Iterate over neighbors of var
         if cons.var2.domain == 1:
-            if cons.is_satisfied(value, cons.var2.value) == False: #If this variable's value breaks the
-                return -1                                      # constraint with a neighbor
+            if cons.is_satisfied(value, cons.var2.value) == False:
+                reduceDomainBy = -1                                 #We can't use this value
         else:
-            numCons +=1
+            numCons += 1                                            #Xj in (Xi,Xj) is un-assigned, increase counter
             counter = 0
             for value2 in cons.var2.domain:
                 if cons.is_satisfied(value, value2) == False:
                     counter += 1
-            if counter == cons.var2.domain:
-                return -1
+            if counter == cons.var2.domain:                         #This value violates every constraint in neighbor
+                reduceDomainBy = -1                                 #We can't use this value
 
-    return 0
+    return reduceDomainBy
 
+#Method that implements the minimum-remaining-values (MRV) and degree heuristic. That is,
+# the variable with the smallest number of values left in its available domain.
+#Input:A csp
+#Output: An iterator that returns the next unassigned variable, ordered by the MRV.
 def select_unassigned_variable(csp):
     """Selects the next unassigned variable, or None if there is no more unassigned variables
     (i.e. the assignment is complete).
@@ -47,8 +57,7 @@ def select_unassigned_variable(csp):
     numCons = 0
 
     unassigned_vars = filter(lambda x: not x.is_assigned(),csp.variables)
-    shortest_len = reduce(lambda x,y: min(x,len(y.domain)),unassigned_vars)
-    mrv_vars = filter(lambda x:len(x.domain) == shortest_len, unassigned_vars)
+    mrv_vars = filter(lambda x:len(x.domain) == len(min(unassigned_vars,key=lambda x:len(x.domain)).domain),unassigned_vars)
 
     gt = -1
     res = None
@@ -60,7 +69,7 @@ def select_unassigned_variable(csp):
             if x + o_x > gt:
                 gt = x+o_x
                 res = var
-
+                print var.domain
     return res 
 
     '''
@@ -82,53 +91,63 @@ def select_unassigned_variable(csp):
         return None
     '''
 
-def is_consistent(csp, variable, val):
-    for cons in csp.constraints[variable]: #Iterate over neighbors of var
-        #print "Size of domain ", len(cons.var2.domain)
-        if cons.var2.domain == 1:
-            if cons.is_satisfied(val, cons.var2.value) == False: #If this variable's value breaks the
+#Checks if a value pertaining to a specific variable violates any constraits  regarding that variable.
+#Input: A constraint satisfaction problem to check for violations, a variable in said csp, and a value of said variable
+#Output: Returns True when the variable assignment to value is consistent, False otherwise
+def is_consistent(csp, variable, value):
+
+    for cons in csp.constraints[variable]:          #Iterate over neighbors of var
+        if cons.var2.domain == 1:                   #var2 is a neighbor variable that we have a constraint with
+            if cons.is_satisfied(value, cons.var2.value) == False: #If this variable's value breaks the
                 return False                                       # constraint with a neighbor
-        else:
+        else:                                       #var2 isn't assigned yet, check all of its values
             counter = 0
             for value2 in cons.var2.domain:
-                if cons.is_satisfied(val, value2) == False:
+                if cons.is_satisfied(value, value2) == False:
                     counter += 1
-            if counter == len(cons.var2.domain):
-                #print "Accept"
+            if counter == len(cons.var2.domain):    #If value violated the constraint for entire domain of var2
                 return False
 
-    #print "Reject"
-    return True
+    return True                                     #value satisfied constraint for at-least 1 value in all neighbors
 
+#Method that checks whether a constraint satisfaction problem has been "solved"
+#Input: A constraint satisfaction problem [Variables, values, constraints]
+#Output: Returns True when the CSP assignment is complete, i.e. all of the variables in the CSP have values assigned,
 def is_complete(csp):
 
     for variable in csp.variables:
-        if variable.is_assigned() == False:
-            return False
+        if variable.is_assigned() == False:   #variable's domain isn't reduced to a single value, and
+            return False                      #doens't have a specific value assigned to it
+
     return True
 
+#Method that returns a list of (ordered) domain values for a given variable by the least-constraining-value heurisitc,
+# that is, the values are ordered in the incresing order of the number of choices for the neighboring variables in
+# the constraint graph
+#Input:A csp, a variable of said csp.
+#Output: an ordered list of variable's values, ordered according to LCV
 def order_domain_values(csp, variable):
-    """Returns a list of (ordered) domain values for the given variable.
 
-    This method implements the least-constraining-value (LCV) heuristic; that is, the values are
-    ordered in the increasing order of the number choices for the neighboring variables in the constraint graph
-    """
     var = variable
     q = PriorityQueue()
 
     for value in var.domain:
         counter = 0
-        for cons in csp.constraints[var]: #Iterate over neighbors of var
+        for cons in csp.constraints[var]:                               #Iterate over neighbors of var
             if cons.var2.is_assigned():
-                if cons.is_satisfied(value, cons.var2.value) == True:
+                if cons.is_satisfied(value, cons.var2.value) == True:   #We found another satisfied constraint
                     counter += 1
-        q.put((counter, value))
+            else:
+                for val2 in cons.var2.domain:
+                    if cons.is_satisfied(value, val2) == True:          #We found another satisfied constraint
+                        counter += 1
+        q.put((counter, value))                                         #Add (# of satisfied cons, value) to queue
 
     orderedDomain = []
     while(q.empty() == False):
-        prio, item = q.get()
-        orderedDomain.append(item)
-
+        counter, value = q.get()
+        orderedDomain.insert(0,value)                                      #add elements of queue to a list
+                                                                         # with correct ordering
     return orderedDomain
 
 def backtracking_search(csp):
@@ -145,99 +164,103 @@ def backtracking_search(csp):
         return None
 
 
+#Method that performs the Backtracking search algorithm on a given csp.
+#Input: a csp to solve.
+#Output: True if a solution to the csp is found, None otherwise.
 def backtrack(csp):
-    """Performs the backtracking search for the given csp.
 
-    If there is a solution, this method returns the successful assignment; otherwise, it returns None.
-    """
+    if is_complete(csp):                            #if all variables in csp are assigned
+        return True
+    var = select_unassigned_variable(csp)           #Get an unassigned variable from the csp
+    for value in order_domain_values(csp, var):     #Iterate over values of the unassigned variable
 
-    if is_complete(csp):
-        #print "Finished"
-        return csp.assignment
-    var = select_unassigned_variable(csp)
-    #print "New var"
-    for value in order_domain_values(csp, var):
-        #print "New value"
-        #print var
-        #print value
-        csp.variables.begin_transaction()
-        if is_consistent(csp, var, value):
-            #print "Got here"
-            csp.variables.begin_transaction()
+        csp.variables.begin_transaction()           #Save your game (in case of incorrect value is chosen)
 
-            csp.assignment[var] = value
+        if is_consistent(csp, var, value):          #Value doesn't violate any constraint with any neighbor
+
+            csp.variables.begin_transaction()       #Save your game again again (needed)
+
+            csp.assignment[var] = value             #update our dictionary
             var.is_assigned() == True
             var.domain = []
-            var.domain.append(value)
+            var.domain.append(value)                #Have the value be the only element in var's domain
 
             inferences = inference(csp, var)
             if inferences != False:
-                csp.assignment[var] = value    #inferences isn't implemented, so this is just a dummy assignment
+                csp.assignment[var] = value         #inferences isn't implemented, so this is just a dummy assignment
                 result = backtrack(csp)
 
-                if result != "Failure":
-                    return csp.assignment
+                if result == True:                  #We found a completed csp
+                    return True
 
-        csp.variables.rollback()
-        csp.assignment[var] = None
+        csp.variables.rollback()                    #Load the game (revert changes to csp variables and domains)
 
-    #print("Backtrack")
-    csp.variables.rollback()
-    return "Failure"
+    csp.variables.rollback()                        #csp has no solution, revert changes we have made to csp
+    return None
 
-#Checks if there is a y in Dj that allows (x,y) to satisfy (Xi, Xj) constraint
-def no_value_salt_is_fries(csp, x, Xi, Xj):
-    foundValue = False
-    for cons in csp.constraints[Xi]: #Iterate over neighbors of var
-        if cons.var2 == Xj and cons.var2.is_assigned():
-            if cons.is_satisfied(x, cons.var2.value) == True: #If this variable's value satisfies the
-                return True                                   # constraint with a neighbor
 
-    return False
+#Method that checks if there is no y in Dj that allows values (x,y) to satisfy a (Xi, Xj) variable constraint
+#Input: a csp, a value x in the domain of variable Xi, variable Xi, variable Xj. where both variables are members
+#   of the given csp.
+#Output: True if the is no value that satisfies any constraint (Xi,Xj) for x, False otherwise
+def no_value_satisfies(csp, x, Xi, Xj):
 
+    for cons in csp.constraints[Xi]:                                #Iterate over neighbors of var
+        if cons.var2 == Xj:                                         #If we are looking at (Xi,Xj) constraints
+            if cons.var2.is_assigned():
+                if cons.is_satisfied(x, cons.var2.value) == True:   #The value does satisfy
+                    return False
+            else:
+                for y in cons.var2.domain:                          #Iterate over Xj values
+                    if cons.is_satisfied(x,y) == True:              #A value does satisfy
+                        return False
+
+    return True
+
+#Method that returns a list of neighbor variables for a variable Xi
+#Input: A csp, a variable Xi that is a member of said csp.
+#Output a list of variables that Xi shares a constraint with.
 def getNeighbors(csp,Xi):
-    neighbors = defaultdict(lambda: None)
+    neighbors = defaultdict(lambda: None)                   #Use a dictionary to store individual variables
+                                                            # b/c there may be more than 1 constraint per var
     for cons in csp.constraints[Xi]:
-        neighbors[cons.var2] = cons.var2
+        neighbors[cons.var2] = cons.var2                    #Record neighbor variable
 
     neighList = []
     for var in neighbors:
-        neighList.append(var)
+        neighList.append(var)                               #Add neighbor variables to a list
 
     return neighList
 
+#Helper method of ac3 that removes all elements from Xi's domain that don't satisfy (Xi,Xj) constraints
+#Input: A csp, a variable Xi in the csp, another variable Xj in the csp
 def revise(csp, Xi, Xj):
-    # You may additionally want to implement the 'revise' method.
+
     revised = False
     for x in Xi.domain:
-        if no_value_salt_is_fries(csp, x, Xi, Xj):
+        if no_value_satisfies(csp, x, Xi, Xj):      #value x doesn't satisfy (Xi,Xj) constraints
             Xi.domain.remove(x)
             revised = True
 
     return revised
 
+#Method that performs the AC-3 (forward checking) algorithm on a csp.
+#Input: A csp, a boolean variable that decides whether the algorithm runs normal AC-3 on the csp,
+# otherwise it starts with only the arcs present in the 'arc' param in the queue
+#Output: True if the arc consistency check succeeds, False otherwise.
 def ac3(csp, arcs=None):
-    """Executes the AC3 or the MAC (p.218 of the textbook) algorithms.
 
-    If the parameter 'arcs' is None, then this method executes AC3 - that is, it will check the arc consistency
-    for all arcs in the CSP.  Otherwise, this method starts with only the arcs present in the 'arcs' parameter
-    in the queue.
-
-    Note that the current domain of each variable can be retrieved by 'variable.domains'.
-
-    This method returns True if the arc consistency check succeeds, and False otherwise."""
-
-    queue_arcs = deque(arcs if arcs is not None else csp.constraints.arcs())
+    queue_arcs = deque(arcs if arcs is not None else csp.constraints.arcs())    #contains (var1,var2) pairings
     while(queue_arcs):
         pair = queue_arcs.pop()
         Xi = pair[0]
         Xj = pair[1]
 
-        listXj = [Xj]
-        if revise(csp,Xi,Xj):
+        listXj = [Xj]                           #create a set containing just the Xj variable
+        if revise(csp,Xi,Xj):                   #If we find values in Xi's domain that don't satisfy (Xi,Xj)
             if len(Xi.domain) == 0:
-                return False
-            for Xk in list(set(getNeighbors(csp,Xi)) - set(listXj)):
-                queue_arcs.append((Xk, Xi))
+                return False                    #csp has no solution with current configuration
+            for Xk in list(set(getNeighbors(csp,Xi)) - set(listXj)):        #iterate over neighbors of Xi that aren't Xj
+                queue_arcs.append((Xk, Xi))     #Add variables with reduced domain to queue
 
     return True
